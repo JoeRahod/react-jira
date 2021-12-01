dbff7474b8b3aa50ee62125afc632644e5d9f127
JoeRahod <631800924@qq.com>
2021-12-01 10:37:20 +0800

    update .gitignore & add costomHook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { useState, useEffect } from "react";
 import qs from "qs";
-import { cleanObject } from "utils";
+import { cleanObject, useMount, useDebounce } from "utils";
 
 const apiUrl = process.env.REACT_APP_API_URL
 
@@ -14,22 +14,23 @@ export const ProjectListScreen = () => {
   });
   const [users, setUsers] = useState([])
   const [list, setList] = useState([])
+  const debounceParam = useDebounce(searchParam, 1000);
   
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(searchParam))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
       if(response.ok) {
         setList(await response.json())
       }  
     })
-  }, [searchParam]);
+  }, [debounceParam]);
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if(response.ok) {
         setUsers(await response.json())
       }  
     })
-  }, []);
+  })
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 // 在一个函数里，改变传入的对象本身是不好的
 export const cleanObject = (object) => {
@@ -11,3 +13,42 @@ export const cleanObject = (object) => {
   });
   return result;
 };
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// export const debounce = (fn, delay) => {
+//   let timeout;
+//   return () => {
+//     if(timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function() {
+//       fn();
+//     }, delay)
+//   }
+// }
+// const log = debounce(() => console.log('debounce'), 1000);
+// log();
+// log();
+// log();
+
+// useDebounce这个costomHook函数作用主要是把value转化成debounceValue;
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value监听变化的时候，设置一个定时器
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    // 每次在上一个useEffect处理之后再运行return的函数
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debounceValue;
+};
+
+// 什么时候用像cleanObject这样的普通函数，什么时候用useDebounce这样的costomHook
+// 关键在于在该函数内是否需要使用到react本身的hook，需要的话则用use开头的函数，即costomHook

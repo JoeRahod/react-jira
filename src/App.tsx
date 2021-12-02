6b370ac136567620aef89f94a7395c1dd7e52bd5
JoeRahod <631800924@qq.com>
2021-12-02 15:01:58 +0800

    feat:新增登录功能,给json-server新增中间件实现模拟登录

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { ProjectListScreen } from "screens/project-list";
+import { LoginScreen } from "screens/login";
 // import {TsReactTest} from './try-use-array';
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
       {/* <TsReactTest /> */}
+      <LoginScreen />
     </div>
   );
 }

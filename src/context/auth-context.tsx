b401f16553ba0acdb343a69767ec0805ff858ce9
JoeRahod <631800924@qq.com>
2021-12-03 10:58:12 +0800

    context hook实现全局登录状态管理 + typescript + react太难理解啦，多看！

---
+++ src/context/auth-context.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode, useState } from "react";
+import { User } from "screens/project-list/search-panel";
+import * as auth from "auth-provider";
+
+interface AuthForm {
+  username: string;
+  password: string;
+}
+
+const AuthContext = React.createContext<
+  | {
+      user: User | null;
+      register: (form: AuthForm) => Promise<void>;
+      login: (form: AuthForm) => Promise<void>;
+      logout: (form: AuthForm) => Promise<void>;
+    }
+  | undefined
+>(undefined);
+
+AuthContext.displayName = "AuthContext"; // 主要用在devtools里，项目中没啥用
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = (form: AuthForm) => auth.login(form).then(setUser); // 消参数 函数式编程point-free
+  const register = (form: AuthForm) =>
+    auth.register(form).then((user) => setUser(user));
+  const logout = (form: AuthForm) => auth.logout().then(() => setUser(null));
+
+  return (
+    <AuthContext.Provider
+      children={children}
+      value={{ user, login, register, logout }}
+    />
+  );
+};
+
+export const useAuth = () => {
+  const context = React.useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth必须在AuthProvider中使用");
+  }
+  return context;
+};

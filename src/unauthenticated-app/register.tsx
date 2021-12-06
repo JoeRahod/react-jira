885e152b8b5f2001728ed2a4bb81cd5cca53df6c
JoeRahod <631800924@qq.com>
2021-12-06 16:45:47 +0800

    安装css in js--emotion-js 美化登录注册页面样式

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,6 +1,7 @@
 import { useAuth } from "context/auth-context";
 import React, { FormEvent } from "react";
 import { Button, Form, Input } from "antd";
+import { LongButton } from "unauthenticated-app";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -26,9 +27,9 @@ export const RegisterScreen = () => {
         <Input placeholder="密码" type="password" id="password" />
       </Form.Item>
       <Form.Item>
-        <Button htmlType="submit" type="primary">
+        <LongButton htmlType="submit" type="primary">
           注册
-        </Button>
+        </LongButton>
       </Form.Item>
     </Form>
   );
